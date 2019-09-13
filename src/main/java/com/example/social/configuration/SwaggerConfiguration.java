fc40bc5d78df777f7d87a36110692f8c6e2b8c8a
Patryk Obiedziński <paobiedzinski@gmail.com>
2019-09-13 13:36:36 +0200

    Swagger configuration available paths added

--- src/main/java/com/example/social/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/social/configuration/SwaggerConfiguration.java
@@ -1,14 +1,22 @@
 package com.example.social.configuration;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Predicates.or;
+import static java.util.stream.Collectors.toList;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +28,7 @@ public class SwaggerConfiguration {
                 .groupName("social-api")
                 .apiInfo(buildApiInfo())
                 .select()
+                .paths(isPathAvailableBySwagger())
                 .apis(RequestHandlerSelectors.any())
                 .build();
     }
@@ -35,5 +44,23 @@ public class SwaggerConfiguration {
                 .licenseUrl("https://example.com")
                 .version("1.0.0")
                 .build();
+    }
+
+    private Predicate<String> isPathAvailableBySwagger() {
+        ArrayList<String> availablePaths = getSwaggerAvailablePaths();
+        List<Predicate<String>> arePathsAvailable = availablePaths.stream()
+                .map(PathSelectors::regex)
+                .collect(toList());
+        return or(arePathsAvailable);
+    }
+
+    private ArrayList<String> getSwaggerAvailablePaths() {
+        ArrayList<String> availablePaths = new ArrayList<>();
+        availablePaths.add("/follows.*");
+        availablePaths.add("/posts.*");
+        availablePaths.add("/timeline.*");
+        availablePaths.add("/user.*");
+        availablePaths.add("/wall.*");
+        return availablePaths;
     }
 }
